d0bc0b07b3c3d06ae8bd7920ee9a94eec23268c4
Arnav Kumar <arunav.jsr.0604@gmail.com>
2026-09-02 20:05:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { HeroSection } from './components/HeroSection';
-import { HeroSection } from './components/HeroSection';
 import { AboutSection } from './components/AboutSection';
 import { ProjectsSection } from './components/ProjectsSection';
 import { SkillsSection } from './components/SkillsSection';
